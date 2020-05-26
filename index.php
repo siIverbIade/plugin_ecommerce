c1482d2131661551ec593c85ee64a9e98b2b53e3
unknown <sergio.silverblade@gmail.com>
2020-05-26 19:40:40 -0300

    teste simples de geolocalização

--- index.php
+++ index.php
@@ -1,23 +1,16 @@
 <?php
-/*
-Plugin Name: NormandieMKT - WMS
-Plugin URI: http://normandiemkt.fr
-Description: WMS PLugin
-Author: NormandieMKT
-Version: 1.0
-*/
 
 declare(strict_types=1);
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
+use App\plugins\geocode\Geocode;
+use App\plugins\geocode\LocationData;
 
-// create a log channel
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler('logs/info.log', Logger::WARNING));
-
-// add records to the log
-$log->warning('Foo');
-$log->error('Bar');
+$ld = new LocationData;
+$ld->setAddress('cep 23067-100');
+$ld->setLocation('BR');
+$gc = new Geocode($ld);
+$gr = $gc->getResponse();
+print_r($gr->geolocation()->getLatitud());
+//print_r($gc->getResponse()->jsonSerialize());
