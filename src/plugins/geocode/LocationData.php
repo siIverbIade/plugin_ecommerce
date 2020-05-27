75a9f9b92ebae0ae7a976020ca039ca1f2a7540b
unknown <sergio.silverblade@gmail.com>
2020-05-27 01:34:41 -0300

    a url aceita apenas 4 tipos de argumentos; os argumentos compostos devem ser tratados dentro do método e codificados na propriedade corespondente;

--- src/plugins/geocode/LocationData.php
+++ src/plugins/geocode/LocationData.php
@@ -5,16 +5,15 @@ namespace App\plugins\geocode;
 class LocationData
 {
     private string $address;
-    private string $location;
-    private string $postal_code;
-    private float $latitud;
-    private float $longitud;
+    private string $components;
+    private string $place_id;
+    private string $latlng;
 
     public function __construct()
     {
     }
 
-    public static function getSingleParameter(string $name, $value): string
+    private static function getSingleParameter(string $name, $value): string
     {
         $encodedValue = urlencode($value);
         return (($value == null) ? '' : "$name=$encodedValue&");
@@ -30,29 +29,61 @@ class LocationData
         return $parameters;
     }
 
+    private function getComponent(string $arg): string
+    {
+        return $this->getComponents()[$arg];
+    }
+
+    private function getComponents(): array
+    {
+        $map = [];
+        if (isset($this->components)) {
+            foreach (explode('|', $this->components) as $value) {
+                $split = explode(':', $value);
+                $map[$split[0]] = $split[1];
+            }
+        }
+        return $map;
+    }
+
     public function getAddress(): string
     {
         return $this->address;
     }
 
-    public function getLocation(): string
+    public function getRegion()
     {
-        return $this->location;
+        return $this->getComponent('region');
+    }
+
+    public function getLocality(): string
+    {
+        return $this->getComponent('locality');
+    }
+
+    public function getCountry(): string
+    {
+        return $this->getComponent('country');
     }
 
     public function getPostalCode(): string
     {
-        return $this->postal_code;
+        return $this->getComponent('postal_code');
     }
 
     public function getLatitud(): string
     {
-        return $this->latitud;
+        return explode(',', $this->latlng)[0];
     }
 
     public function getLongitud(): string
     {
-        return $this->longitud;
+        return explode(',', $this->latlng)[1];
+    }
+
+    public function getPlaceId()
+    {
+        return $this->place_id;
     }
 
     public function setAddress(string $address)
@@ -60,23 +91,70 @@ class LocationData
         $this->address = $address;
     }
 
-    public function setLocation(string $location)
+    private function setComponents(array $components)
     {
-        $this->location = $location;
+        $map = [];
+        foreach ($components as $ind => $val) {
+            $map[] = "$ind:$val";
+        }
+        $this->components = implode('|', $map);
+    }
+
+    private function setComponent(string $name, string $value)
+    {
+        $array = $this->getComponents();
+        if (!key_exists($name, $array)) {
+            $array[$name] = $value;
+        } else {
+            if ($value == '') unset($array[$name]);
+        }
+        $this->setComponents($array);
+    }
+
+    public function setRegion(string $region)
+    {
+        $this->setComponent('region', $region);
+    }
+
+    public function setLocality(string $locality)
+    {
+        $this->setComponent('locality', $locality);
+    }
+
+    public function setCountry(string $country)
+    {
+        $this->setComponent('country', $country);
     }
 
     public function setPostalCode(string $postalCode)
     {
-        $this->postal_code = $postalCode;
+        $this->setComponent('postal_code', $postalCode);
     }
 
     public function setLatitud(float $latitud)
     {
-        $this->latitud = $latitud;
+        if (isset($this->latlng)) {
+            $latlng = explode(',', $this->latlng);
+        } else {
+            $latlng = [];
+        }
+        $latlng[0] = $latitud;
+        $this->latlng = implode(',', $latlng);
     }
 
     public function setlongitud(float $longitud)
     {
-        $this->longitud = $longitud;
+        if (isset($this->latlng)) {
+            $latlng = explode(',', $this->latlng);
+        } else {
+            $latlng = [];
+        }
+        $latlng[1] = $longitud;
+        $this->latlng = implode(',', $latlng);
+    }
+
+    public function setPlaceId(string $id)
+    {
+        $this->place_id = $id;
     }
 }
